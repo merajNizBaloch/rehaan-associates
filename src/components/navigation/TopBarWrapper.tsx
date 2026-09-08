365c11138e4f7c0c2f74cbf95d0fb224b2681d91
Meraj Niaz Baloch <90909350+merajNizBaloch@users.noreply.github.com>
2026-09-09 01:04:40 +0500

    Remove fixed hero animation overlays

--- src/components/navigation/TopBarWrapper.tsx
+++ src/components/navigation/TopBarWrapper.tsx
@@ -1,25 +1,18 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-
-import CapabilitiesHeroMotion from "../CapabilitiesHeroMotion";
 import EnquiryFormBridge from "../EnquiryFormBridge";
-import HomeHeroConstructionMotion from "../HomeHeroConstructionMotion";
 import PublicHoverEnhancer from "../PublicHoverEnhancer";
 import { useSiteMode } from "../SiteModeProvider";
 import TopBarV2 from "./TopBarV2";
 
 export default function TopBarWrapper() {
   const { mode, setMode } = useSiteMode();
-  const pathname = usePathname();
 
   return (
     <>
       <EnquiryFormBridge />
       <TopBarV2 mode={mode} setMode={setMode} />
       <PublicHoverEnhancer />
-      {pathname === "/" ? <HomeHeroConstructionMotion mode={mode} /> : null}
-      {pathname === "/capabilities" ? <CapabilitiesHeroMotion mode={mode} /> : null}
     </>
   );
 }
