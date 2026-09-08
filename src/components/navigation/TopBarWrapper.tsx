b2e8e81b1a62d3e87e39f121af317cbc31b7da0b
Meraj Niaz Baloch <90909350+merajNizBaloch@users.noreply.github.com>
2026-09-09 00:49:56 +0500

    Show construction animation on homepage hero

--- src/components/navigation/TopBarWrapper.tsx
+++ src/components/navigation/TopBarWrapper.tsx
@@ -1,16 +1,21 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+
 import EnquiryFormBridge from "../EnquiryFormBridge";
+import HomeHeroConstructionMotion from "../HomeHeroConstructionMotion";
 import { useSiteMode } from "../SiteModeProvider";
 import TopBarV2 from "./TopBarV2";
 
 export default function TopBarWrapper() {
   const { mode, setMode } = useSiteMode();
+  const pathname = usePathname();
 
   return (
     <>
       <EnquiryFormBridge />
       <TopBarV2 mode={mode} setMode={setMode} />
+      {pathname === "/" ? <HomeHeroConstructionMotion mode={mode} /> : null}
     </>
   );
 }
